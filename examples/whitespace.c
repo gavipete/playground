1e5656e1a3fb721cd93671586546f2f7ce05a588
Gavin Peters <gavin.peters@todaqfinance.com>
2021-12-03 11:53:11 +0300

    add more

--- examples/whitespace.c
+++ examples/whitespace.c
@@ -1,5 +1,32 @@
 #include <stdio.h>     
 
+/** 
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+ * eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+ * minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+ * aliquip ex ea commodo consequat. Duis aute irure dolor in
+ * reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+ * pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+ * culpa qui officia deserunt mollit anim id est laborum.
+ */
+
+/*****
+
+ACHTUNG!
+
+ALLES TURISTEN UND NONTEKNISCHEN LOOKENSPEEPERS!
+
+DAS KOMPUTERMASCHINE IST NICHT FÜR DER GEFINGERPOKEN UND MITTENGRABEN!
+
+ODERWISE IST EASY TO SCHNAPPEN DER SPRINGENWERK, BLOWENFUSEN UND
+POPPENCORKEN MIT SPITZENSPARKEN.  IST NICHT FÜR GEWERKEN BEI
+DUMMKOPFEN. DER RUBBERNECKEN SIGHTSEEREN KEEPEN DAS COTTONPICKEN
+HÄNDER IN DAS POCKETS MUSS.
+
+ZO RELAXEN UND WATSCHEN DER BLINKENLICHTEN!!!   
+
+ *****/
+
 int main(int argc, char* argv[]) {
     printf("Hello, World.\n");
 }
